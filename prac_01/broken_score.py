29a343319ff8ba6e4d61be3b7b3cfb98b9de46d3
emily-vangorp <emily.vangorp@my.jcu.edu.au>
2018-08-15 08:24:15 +1000

    Refactor broken score and temperatures programs to use functions

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -1,9 +1,17 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = get_score()
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+main()
