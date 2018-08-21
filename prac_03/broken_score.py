4ba510807a75958905888efbed94066b29e97bb8
emily-vangorp <42363309+emily-vangorp@users.noreply.github.com>
2018-08-21 14:49:45 +1000

    Update broken_score.py

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,11 +1,10 @@
 def main():
-    result = get_result()
+    score = float(input("Enter score: "))
+    result = get_result(score)
     print(result)
    
 
-
-def get_result():
-    score = float(input("Enter score: "))
+def get_result(score):
      if score < 0 or score > 100:
         result = "Invalid score"
     elif score >= 90:
