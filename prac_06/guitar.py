cd84b440ea6c8b7af73feacb296b0dbac169cfc0
emily-vangorp <emily.vangorp@my.jcu.edu.au>
2018-09-11 10:22:23 +1000

    update guitar exercise

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,7 @@
+CURRENT_YEAR = 2018
+VINTAGE_AGE = 50
+
+
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
@@ -8,11 +12,10 @@ class Guitar:
         return "{} ({}) : ${:10,.2f}".format(self.name, self.year, self.cost)
 
     def get_age(self):
-        age = 2018 - self.year
-        return age
+        return CURRENT_YEAR - self.year
 
     def is_vintage(self):
-        if self.get_age() >= 50:
-            return True
-        else:
-            return False
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        return self.year < other.year
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,21 +1,40 @@
 from prac_06.guitar import Guitar
 
-my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-another_guitar = Guitar("Another Guitar", 2012, 6050.95)
+
+# my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+# another_guitar = Guitar("Another Guitar", 2012, 6050.95)
 # print("{} get_age() - Expected 96. Got {}".format(my_guitar.name, my_guitar.get_age()))
 # print("{} get_age() - Expected 6. Got {}".format(another_guitar.name, another_guitar.get_age()))
 # print("{} is_vintage() - Expected True. Got {}".format(my_guitar.name, my_guitar.is_vintage()))
 # print("{} is_vintage() - Expected False. Got {}".format(another_guitar.name, another_guitar.is_vintage()))
 
-print("My guitars!")
-guitar = Guitar
-guitars = []
-while guitar.name != "":
-    guitar.name = input("Name: ")
-    guitar.year = int(input("Year: "))
-    guitar.cost = float(input("Cost: $"))
-    print("{} ({}) : ${} added.".format(guitar.name, guitar.year, guitar.cost))
-    guitars.append(guitar)
-print(guitars)
+def main():
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Another Guitar", 2012, 6050.95))
+
+    guitars.sort()
+    print("These are my guitars:")
+
+    if guitars is not None:
+        for i, guitar in enumerate(guitars):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {0}: {1.name:>30} ({1.year}), worth ${1.cost:10,.2f}\
+                  {2}".format(i + 1, guitar, vintage_string))
+    else:
+        print("No guitars :( Quick go and buy one!")
 
 
+main()
